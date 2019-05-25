bfc5c0d074a2feb204de50d230ad24b8e2389165
“Patrick <pctennislessons@gmail.com>
2019-05-25 16:02:00 -0600

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def d 
+dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&",
+}
+dictionary
+end
+def word_substituter(tweet)
+    tweet.split(" ").map do |x|
+        if d.include?(x.downcase)
+            d[x.downcase]
+        else
+            x
+        end
+    end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+    tweets.each do |x|
+       puts word_substituter(x)
+    end
+end
+
+def selective_tweet_shortener(tweets)
+    put tweets
+end
